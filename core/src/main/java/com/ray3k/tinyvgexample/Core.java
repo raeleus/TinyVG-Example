1094909e233373299d9e5f369eaac1d8e1abf123
raeleus <raymond.ray3k@gmail.com>
2022-04-12 17:08:56 -0700

    Initial commit

---
+++ core/src/main/java/com/ray3k/tinyvgexample/Core.java
@@ -0,0 +1,52 @@
+package com.ray3k.tinyvgexample;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import dev.lyze.gdxtinyvg.TinyVG;
+import dev.lyze.gdxtinyvg.TinyVGAssetLoader;
+import dev.lyze.gdxtinyvg.drawers.TinyVGShapeDrawer;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Core extends ApplicationAdapter {
+	private TinyVG tvg;
+	private TinyVGShapeDrawer drawer;
+	private Viewport viewport = new ScreenViewport();
+
+	@Override
+	public void create() {
+		TinyVGAssetLoader assetLoader = new TinyVGAssetLoader();
+		tvg = assetLoader.load("pig.tvg");
+		
+		drawer = new TinyVGShapeDrawer(new SpriteBatch());
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0.25f, 0.25f, 0.25f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		viewport.apply();
+		drawer.getBatch().setProjectionMatrix(viewport.getCamera().combined);
+		
+		drawer.getBatch().begin();
+		tvg.draw(drawer);
+		drawer.getBatch().end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height, true);
+	}
+	
+	@Override
+	public void dispose() {
+
+	}
+}
---
+++ lwjgl3/src/main/java/com/ray3k/tinyvgexample/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,27 @@
+package com.ray3k.tinyvgexample.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.ray3k.tinyvgexample.Core;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new Core(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("tinyvgexample");
+		configuration.useVsync(true);
+		configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+		configuration.setWindowedMode(640, 480);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		configuration.setBackBufferConfig(8, 8, 8, 8, 16, 2, 4);
+		return configuration;
+	}
+}
